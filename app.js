2bc660e7756b1ec25932dd5091cc66e2a96b68b0
mostafa123c <mostafaemad112744@gmail.com>
2023-07-07 22:03:51 +0300

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+require("dotenv").config()
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(cookieParser())
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://mostafa:momo1127447085@cluster0.xv82euo.mongodb.net/node-auth';
+const dbURI = process.env.MONGO_URI;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
